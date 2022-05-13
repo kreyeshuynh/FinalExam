ce23345fa1b84c3c7c74b24370653981c4299c81
tacos <kaianu@hawaii.edu>
2022-05-13 01:02:56 -1000

    Initial

---
+++ cmake-build-debug/CMakeFiles/FinalExam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalExam.dir/main.cpp.o"
+  "CMakeFiles/FinalExam.dir/main.cpp.o.d"
+  "FinalExam"
+  "FinalExam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FinalExam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
